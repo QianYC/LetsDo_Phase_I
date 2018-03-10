4df0806b66091216cebc942bbd18cac3e8241e7f
161250103 <1767855130@qq.com>
2018-03-10 16:41:34 +0800

    modify test case

--- src/test/java/YingYingMonster/LetsDo_Phase_I/Test_MockController.java
+++ src/test/java/YingYingMonster/LetsDo_Phase_I/Test_MockController.java
@@ -9,9 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.DEFINED_PORT)
 public class Test_MockController {
 
