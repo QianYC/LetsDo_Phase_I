496328b80c0a44e27862ffa43be1d5c3d4b044f8
161250103 <1767855130@qq.com>
2018-03-11 19:08:10 +0800

    fix bugs in test

--- src/test/java/YingYingMonster/LetsDo_Phase_I/Test_MockController.java
+++ src/test/java/YingYingMonster/LetsDo_Phase_I/Test_MockController.java
@@ -1,7 +1,11 @@
 package YingYingMonster.LetsDo_Phase_I;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,24 +13,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.DEFINED_PORT)
+//@WebAppConfiguration
 public class Test_MockController {
 
-	@Autowired
-	private TestRestTemplate trt;
+	private MockMvc mvc;
 	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	@Before
+	public void setUp() throws Exception {
+		mvc=new MockMvcBuilders().standaloneSetup(new MockController()).build();
 	}
 
 	@Test
-	public void test() {
-		String msg=this.trt.getForObject("/greetings", String.class);
-		assertEquals(msg,"hello travis!");
+	public void test() throws Exception{
+		mvc.perform(MockMvcRequestBuilders.get("/greetings").accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(content().string(equalTo("hello travis!")));
 	}
 
 }
