0ff49f29fcd0874bc15af74fcac37ab3e3a75a83
161250103 <1767855130@qq.com>
2018-03-15 23:52:27 +0800

    初步定义api

--- src/main/java/YingYingMonster/LetsDo_Phase_I/controller/UserController.java
+++ src/main/java/YingYingMonster/LetsDo_Phase_I/controller/UserController.java
@@ -7,18 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import YingYingMonster.LetsDo_Phase_I.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @Controller
 @RequestMapping("/user")
+@Api()
 public class UserController {
 	
 	@PostMapping("/register")
+	@ApiOperation(value = "注册新用户，注册成功后跳转至登录界面")
 	public String register(@ModelAttribute("user")User user){
 		
 		return null;
 	}
 	
 	@PostMapping("/login")
+	@ApiOperation(value = "用户登录，成功后返回用户主界面")
 	public String login(@RequestParam("username")String username
 			,@RequestParam("password")String password){
 		
@@ -26,6 +31,7 @@ public class UserController {
 	}
 	
 	@PostMapping("/modify")
+	@ApiOperation(value = "用户修改自身信息，不做页面跳转")
 	public String modify(@ModelAttribute("user")User user){
 		
 		return null;
