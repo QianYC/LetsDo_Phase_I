52c7f3c505c0289ed3e78aa14867fa4772f68742
161250103 <1767855130@qq.com>
2018-03-16 14:23:10 +0800

    UserController添加两个get方法

--- src/main/java/YingYingMonster/LetsDo_Phase_I/controller/UserController.java
+++ src/main/java/YingYingMonster/LetsDo_Phase_I/controller/UserController.java
@@ -1,6 +1,7 @@
 package YingYingMonster.LetsDo_Phase_I.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +20,22 @@ public class UserController {
 	
 	UserDAO userImpl=new UserDAOImpl();
 	
+	@GetMapping("/register")
+	@ApiOperation(value = "访问用户注册界面")
+	public String visitRegisterPage(){
+		return "register";
+	}
+	
 	@PostMapping("/register")
 	@ApiOperation(value = "注册新用户，注册成功后跳转至登录界面")
 	public String register(@ModelAttribute("user")User user){
 		userImpl.register(user.getId(),user.getPw());
+		return "redirect:/user/login";
+	}
+	
+	@GetMapping("/login")
+	@ApiOperation(value = "访问用户登录界面")
+	public String visitLoginPage(){
 		return "login";
 	}
 	
